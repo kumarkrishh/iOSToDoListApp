cb0770f268cd06ebc6e6b4316655ae78b8905c68
Krish Kumar <krishs.kumar14@gmail.com>
2024-09-21 23:38:05 -0700

    finished CRUD operations for to do list creation

--- ViewModels/ProfileViewViewModel.swift
+++ ViewModels/ProfileViewViewModel.swift
@@ -4,13 +4,26 @@
 //
 //  Created by Krish on 9/20/24.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ProfileViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }
