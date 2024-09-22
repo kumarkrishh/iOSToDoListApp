bb1f05657fc6809e644610227fe374e2b2a11e3b
Krish Kumar <krishs.kumar14@gmail.com>
2024-09-21 23:26:25 -0700

    added create/delete to do list item functionality

--- ViewModels/ToDoListViewViewModel.swift
+++ ViewModels/ToDoListViewViewModel.swift
@@ -6,9 +6,28 @@
 //
 
 import Foundation
+import FirebaseFirestore
 
 /// ViewModel for list of items view
 /// Primary tab
 class ToDoListViewViewModel: ObservableObject {
-    init() {}
+    @Published var showingNewItemView = false
+    
+    private var userId: String
+    
+    init(userId: String) {
+        self.userId = userId
+    }
+    
+    /// Delete to do list item
+    /// - Parameter id: Item id to delete
+    func delete(id: String) {
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(userId)
+            .collection("todos")
+            .document(id)
+            .delete()
+    }
 }
--- Views/ToDoListView.swift
+++ Views/ToDoListView.swift
@@ -6,33 +6,47 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
 
 struct ToDoListView: View {
-    @StateObject var viewModel = ToDoListViewViewModel()
-    
-    private let userId: String
+    @StateObject var viewModel : ToDoListViewViewModel
+    @FirestoreQuery var items: [ToDoListItem]
     
     init(userId: String) {
-        self.userId = userId
+        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
+        
+        self._viewModel = StateObject(wrappedValue: ToDoListViewViewModel(userId: userId))
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                
+                List(items) { item in
+                    ToDoListItemView(item: item)
+                        .swipeActions {
+                            Button("Delete") {
+                                viewModel.delete(id: item.id)
+                            }
+                            .tint(.red)
+                        }
+                }
+                .listStyle(PlainListStyle())
             }
             .navigationTitle("To Do List")
             .toolbar {
                 Button {
-                    // Action
+                    viewModel.showingNewItemView = true
                 } label: {
                     Image(systemName: "plus")
                 }
+            }
+            .sheet(isPresented: $viewModel.showingNewItemView) {
+                NewItemView(newItemPresented: $viewModel.showingNewItemView)
             }
         }
     }
 }
 
 #Preview {
-    ToDoListView(userId: "")
+    ToDoListView(userId: "AGtLkHa348gQ79mTWAl2NeIWJIu1")
 }
